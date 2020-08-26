888add68d6cfd340fdc877ed560b0e7056373127
idawnwon <69025059+idawnwon@users.noreply.github.com>
2020-08-26 15:17:12 +0800

    Update install_docker_and_compose.sh

--- bin/install_docker_and_compose.sh
+++ bin/install_docker_and_compose.sh
@@ -38,16 +38,11 @@ user_in_docker_group=$(id -nG "$USER" | grep docker)
 if [ -z "$docker_group" ] && [ -z "$user_in_docker_group" ]; then
   sudo groupadd docker
   sudo usermod -aG docker $USER
-  newgrp docker
 elif [ -z "$user_in_docker_group" ]; then
   sudo usermod -aG docker $USER
-  newgrp docker
 fi
 
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
-
-
-
